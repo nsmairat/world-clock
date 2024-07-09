bf208ff23496c5160f8cea2ac82e990783d1582c
nsmairat <nilasmairat@gmail.com>
2024-07-08 23:15:05 -0700

    Added the functionality for the Select element on js

--- javascript/index.js
+++ javascript/index.js
@@ -23,14 +23,28 @@ setInterval(function () {
 
   moscowDateElement.innerHTML = moscowTime.format("MMMM Do YYYY");
   moscowTimeElement.innerHTML = moscowTime.format(
-    "h:mm:ss [<small>]A[</small>]"
+    "h:mm:ss [<small>]A[</small>]" //)}<small>${lasVegasTime.format("A")}</small>`;
   );
 }, 1000);
 
-//las vegas
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss [<small>]A[</small>]"
+          )}</div>
+        </div>
+  
+  `;
+}
 
-//)}<small>${lasVegasTime.format("A")}</small>`;
-
-//milan
-
-//moscow
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
